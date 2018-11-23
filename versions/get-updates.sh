23fa37021a236f682dec1340bba60fa498d06037
juan.arboleda <Juan.x.Arboleda.-nd@disney.com>
2018-11-23 07:46:23 -0500

    Migrate message

--- versions/get-updates.sh
+++ versions/get-updates.sh
@@ -1,1 +1,2 @@
-find */*/src/ -name *.json | awk '{print "\"" $0 "\""}' | xargs ls -l --time-style="+%Y-%m-%d %H:%M:00" | awk '{print $8 " " $6 " " $7}' | awk -F "/" '{print $6}' | sort | uniq | awk '{print "\"" $1 "\"\t:" "\"" $2 " " $3 "\","}'
+touch app-resources/versions/updates-index.json
+find app-resources -name *.json | awk '{print "\"" $0 "\""}' | xargs ls -ltr --time-style="+%Y-%m-%d %H:%M:00" | awk '{print $8 " " $6 " " $7}' | awk -F "/" '{print $3}' | sort | uniq | awk '{print "\"" $1 "\" : " "\"" $2 " " $3 "\","}'
